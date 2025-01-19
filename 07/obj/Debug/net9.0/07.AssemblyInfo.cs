77f3d63b9e78de8d5b4f95852ace484c543c8952
yves848 <yves.godart@gmail.com>
2025-01-19 20:57:54 +0100

    07-2

--- 07/obj/Debug/net9.0/07.AssemblyInfo.cs
+++ 07/obj/Debug/net9.0/07.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("07")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+223d663d0b5123b443b151027095a68677c345ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69efe91472b4229425e19d880903d18d162cf45")]
 [assembly: System.Reflection.AssemblyProductAttribute("07")]
 [assembly: System.Reflection.AssemblyTitleAttribute("07")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
