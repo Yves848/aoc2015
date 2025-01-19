6ee0f137ed1ee29594478f1fb7d90d99ffa229cb
yves848 <yves.godart@gmail.com>
2025-01-19 10:10:38 +0100

    02-02

---
+++ 01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+var file = args.Length > 0 ? File.ReadAllText(args[0]) : File.ReadAllText($"{home}/git/aoc2015/01/test.txt");
+
+void part1()
+{
+  int ans = 0;
+  int open = file.ToList().Where(c => c == '(').Count();
+  int close = (file.Length - open)*-1;
+  ans = open + close;
+  Console.WriteLine($"Part 1 - Answer : {ans}");
+}
+
+void print(string str, bool valid)
+{
+  if (valid)
+  {
+    Console.ForegroundColor = ConsoleColor.Green;
+  }
+  else
+  {
+    Console.ForegroundColor = ConsoleColor.Red;
+  }
+  Console.Write($"{str} ");
+}
+
+void part2()
+{
+  int ans = 0;
+  for(int i = 0; i < file.Length; i++) {
+    ans +=  file.Substring(i,1) == "(" ? 1 : -1;
+    if (ans == -1) {
+       ans = i+1;
+       break;
+    }
+  }
+  Console.WriteLine($"Part 2 - Answer : {ans}");
+}
+
+part1();
+
+part2();
---
+++ 02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.RegularExpressions;
+
+using System.Runtime.InteropServices;
+
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2015/02/test.txt").ToList();
+var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
+//l x w x h
+void part1()
+{
+  Regex re = new(@"\d+");
+  int ans = 0;
+  file.ForEach(line =>
+  {
+    MatchCollection d = re.Matches(line);
+    var (l, w, h) = (int.Parse(d[0].Value), int.Parse(d[1].Value), int.Parse(d[2].Value));
+    ans += (2 * l * w + 2 * w * h + 2 * h * l);
+    List<int> dim = [l * w, w * h, h * l];
+    ans += dim.Min();
+  });
+  Console.WriteLine($"Part 1 - Answer : {ans}");
+}
+
+void print(string str, bool valid)
+{
+  if (valid)
+  {
+    Console.ForegroundColor = ConsoleColor.Green;
+  }
+  else
+  {
+    Console.ForegroundColor = ConsoleColor.Red;
+  }
+  Console.Write($"{str} ");
+}
+
+void part2()
+{
+  Regex re = new(@"\d+");
+  int ans = 0;
+  file.ForEach(line =>
+  {
+    MatchCollection d = re.Matches(line);
+    var (l, w, h) = (int.Parse(d[0].Value), int.Parse(d[1].Value), int.Parse(d[2].Value));
+    List<int> di = [l,w,h];
+    di.Sort();
+    var p1 = di[0]+di[0]+di[1]+di[1];
+    var p2 = l*w*h;
+    ans += p1+p2;
+    
+  });
+  Console.WriteLine($"Part 2 - Answer : {ans}");
+}
+
+part1();
+
+part2();
