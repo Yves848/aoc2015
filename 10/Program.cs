8ab06d7aaa4719ecf73b63eac97d4f023810aa59
yves848 <yves.godart@gmail.com>
2025-01-22 01:28:04 +0100

    10 WIP

---
+++ 10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+// List<string> file = args.Length > 0 ? File.ReadAllLines(args[0]).ToList() : File.ReadAllLines($"{home}/git/aoc2015/10/data.txt").ToList();
+// var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
+
+// string puzzle = "1"; // test
+string puzzle = "1113122113"; // real
+
+
+void part1()
+{
+  int ans = 0;
+
+  for (int i = 0; i < 50; i++)
+  {
+    string result = "";
+    char currentChar = puzzle[0];
+    string currentGroup = "";
+    string temp = "";
+    foreach (char c in puzzle)
+    {
+      if (c == currentChar)
+      {
+        currentGroup += c;
+      }
+      else
+      {
+        //result.Add(currentGroup);
+        result += currentGroup.Length.ToString()+currentGroup[0];
+        currentGroup = c.ToString();
+        currentChar = c;
+      }
+    }
+
+    result += currentGroup.Length.ToString()+currentGroup[0];
+    puzzle = result;
+  }
+  ans = puzzle.Length;
+  Console.WriteLine($"Part 1 - Answer : {ans}");
+}
+
+void part2()
+{
+  int ans = 0;
+  Console.WriteLine($"Part 2 - Answer : {ans}");
+}
+
+part1();
+
+part2();
