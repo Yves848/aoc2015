8a8977607f3cdbb7fbcf6b4219be4f5d63e953e9
yves848 <yves.godart@gmail.com>
2025-01-23 23:16:19 +0100

    11

---
+++ 11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+string puzzle = "hepxcrrq";
+
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+List<string> triple = [];
+for (int i = 0; i < alphabet.Length-2; i++){
+  triple.Add(alphabet.Substring(i,3));
+}
+
+string test = "dkfjdskmjfsdmlkabcmlsùmdlsùp";
+
+List<string> triple2 = [];
+for (int i = 0; i < test.Length-2; i++){
+  triple2.Add(test.Substring(i,3));
+}
+
+var u = triple.Intersect(triple2);
+Console.WriteLine(u);
+// triple.ForEach(f => Console.WriteLine(f));
