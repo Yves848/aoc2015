39c03420894de664263b4cfba4f079e95f621c23
yves848 <yves.godart@gmail.com>
2025-01-24 14:57:10 +0100

    11-2

--- 11/Program.cs
+++ 11/Program.cs
@@ -1,20 +1,99 @@
-﻿using System.Security.Cryptography;
-
-string puzzle = "hepxcrrq";
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.IO.Pipelines;
+using System.Text.RegularExpressions;
 
 string alphabet = "abcdefghijklmnopqrstuvwxyz";
 List<string> triple = [];
-for (int i = 0; i < alphabet.Length-2; i++){
-  triple.Add(alphabet.Substring(i,3));
+for (int i = 0; i < alphabet.Length - 2; i++)
+{
+  triple.Add(alphabet.Substring(i, 3));
 }
 
-string test = "dkfjdskmjfsdmlkabcmlsùmdlsùp";
+string puzzle = "hepxcrrq";
 
-List<string> triple2 = [];
-for (int i = 0; i < test.Length-2; i++){
-  triple2.Add(test.Substring(i,3));
+string increment(string pwd)
+{
+  int index = pwd.Length - 1;
+  while (true)
+  {
+    char c = pwd[index];
+    c++;
+    if (c > 'z')
+    {
+      pwd = pwd.Remove(index, 1).Insert(index, "a");
+      index--;
+    }
+    else
+    {
+      if ("iol".Contains(c.ToString())) c++;
+      pwd = pwd.Remove(index, 1).Insert(index, c.ToString());
+      break;
+    }
+  }
+  return pwd;
 }
 
-var u = triple.Intersect(triple2);
-Console.WriteLine(u);
+bool triplet(string pwd)
+{
+  List<string> t = [];
+  for (int i = 0; i < pwd.Length - 2; i++)
+  {
+    t.Add(pwd.Substring(i, 3));
+  }
+  return triple.Intersect(t).ToList().Count() > 0;
+}
+
+bool pairs(string pwd) {
+  Regex re = new(@"(\w)\1");
+  var matches = re.Matches(pwd);
+  HashSet<string> p = [];
+  matches.ToList().ForEach(m=> {
+    p.Add(m.Value);
+  });
+  return p.Count > 1;
+}
+
+void part1()
+{
+  bool correct = false;
+  while (!correct)
+  {
+    puzzle = increment(puzzle);
+    correct = triplet(puzzle);
+    correct = correct && !puzzle.Contains("i");
+    correct = correct && !puzzle.Contains("o");
+    correct = correct && !puzzle.Contains("l");
+    correct = correct && pairs(puzzle);
+  }
+      Console.WriteLine(puzzle);
+}
+
+void part2()
+{
+  puzzle = "hepxcrrq";
+  bool correct = false;
+  int i = 0;
+  while (!correct)
+  {
+    puzzle = increment(puzzle);
+    correct = triplet(puzzle);
+    correct = correct && !puzzle.Contains("i");
+    correct = correct && !puzzle.Contains("o");
+    correct = correct && !puzzle.Contains("l");
+    correct = correct && pairs(puzzle);
+    if (correct && i == 0) {
+      correct = false;
+      i++;
+    }
+  }
+      Console.WriteLine(puzzle);
+}
+
+part1();
+
+part2();
+
+// var u = triple.Intersect(triple2).ToList();
+
 // triple.ForEach(f => Console.WriteLine(f));
--- 11/obj/Debug/net9.0/11.AssemblyInfo.cs
+++ 11/obj/Debug/net9.0/11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a586fbd84caf7e3e7a7264fce870d9b6d54d67b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a8977607f3cdbb7fbcf6b4219be4f5d63e953e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
