d4ab54cb834a76b20f7be8a1904d2a94fdab973f
yves848 <yves.godart@gmail.com>
2025-01-27 01:51:22 +0100

    19 WIP

--- 19/Program.cs
+++ 19/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+string home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+string file = args.Length > 0 ? File.ReadAllText(args[0]) : File.ReadAllText($"{home}/git/aoc2015/19/test.txt");
+var lf = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\r\n" : "\n";
+var blocs = file.Split($"{lf}{lf}");
+
+void part1()
+{
+  int ans = 0;
+  Console.WriteLine($"Part 1 - Answer : {ans}");
+}
+
+void part2()
+{
+  int ans = 0;
+  Console.WriteLine($"Part 2 - Answer : {ans}");
+}
+
+part1();
+
+part2();
